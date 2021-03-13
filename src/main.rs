0540f73f26849a5919d0c6e4434ee648f7d4f095
Kirill Nikitin <locke23rus@gmail.com>
2021-03-13 20:14:02 +0200

    Implement downloading and extracting of crates

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 extern crate clap;
+extern crate reqwest;
+
 use clap::{App, Arg};
-use std::env;
-use std::fs;
-use std::io;
+use reqwest::header::CONTENT_LENGTH;
+use std::io::{self, Read};
 use std::path::PathBuf;
+use std::{env, error::Error};
+use std::{fs, process::exit};
 
 fn main() {
     let matches = App::new("cargo-diff")
@@ -33,11 +36,22 @@ fn main() {
     let version2 = matches.value_of("VERSION_2").unwrap();
 
     let temp_dir = create_temp_dir().unwrap();
-    let crate1 = download_crate(temp_dir.clone(), package_name, version1).unwrap();
-    let crate2 = download_crate(temp_dir.clone(), package_name, version2).unwrap();
+    let crate_dir1 = download_and_extract_crate(
+        temp_dir.clone(),
+        package_name.to_string(),
+        version1.to_string(),
+    )
+    .unwrap();
 
-    println!("{}", crate1.display());
-    println!("{}", crate2.display());
+    let crate_dir2 = download_and_extract_crate(
+        temp_dir.clone(),
+        package_name.to_string(),
+        version2.to_string(),
+    )
+    .unwrap();
+
+    println!("{}", crate_dir1.display());
+    println!("{}", crate_dir2.display());
 }
 
 fn create_temp_dir() -> io::Result<PathBuf> {
@@ -49,14 +63,67 @@ fn create_temp_dir() -> io::Result<PathBuf> {
     Ok(dir)
 }
 
-fn download_crate(dir: PathBuf, name: &str, version: &str) -> Result<PathBuf, String> {
-    let mut crate_path = dir.clone();
-    crate_path.push(format!("{}-{}.crate", name, version));
+fn download_and_extract_crate(
+    dir: PathBuf,
+    name: String,
+    version: String,
+) -> Result<PathBuf, String> {
+    let crate_bytes = download_crate(name.clone(), version.clone()).unwrap_or_else(|e| {
+        println!(
+            "Failed to download crate `{}={}`: {}",
+            name.clone(),
+            version.clone(),
+            e
+        );
+        exit(1);
+    });
 
-    let url = format!(
+    println!("Extracting crate archive to {}/", dir.display());
+    let gzip = flate2::read::GzDecoder::new(&crate_bytes[..]);
+    let mut archive = tar::Archive::new(gzip);
+    match archive.unpack(dir.clone()) {
+        Ok(_) => {
+            println!("Crate content extracted to {}/", dir.display());
+        }
+        Err(e) => {
+            println!("Couldn't extract crate to {}/: {}", dir.display(), e);
+            exit(1)
+        }
+    }
+
+    let mut crate_dir = dir.clone();
+    crate_dir.push(format!("{}-{}", name, version));
+
+    Ok(crate_dir)
+}
+
+/// Download given crate and return it as a vector of gzipped bytes.
+fn download_crate(name: String, version: String) -> Result<Vec<u8>, Box<dyn Error>> {
+    let download_url = format!(
         "https://crates.io/api/v1/crates/{}/{}/download",
         name, version
     );
-    println!("{}", url);
-    Ok(crate_path)
+    println!(
+        "Downloading crate `{}={}` from {}",
+        name, version, download_url
+    );
+    let mut response = reqwest::blocking::get(&download_url)?;
+
+    let content_length: Option<usize> = response
+        .headers()
+        .get(CONTENT_LENGTH)
+        .and_then(|ct_len| ct_len.to_str().ok())
+        .and_then(|ct_len| ct_len.parse().ok());
+    println!(
+        "Download size: {}",
+        content_length.map_or("<unknown>".into(), |cl| format!("{} bytes", cl))
+    );
+    let mut bytes = match content_length {
+        Some(cl) => Vec::with_capacity(cl),
+        None => Vec::new(),
+    };
+    response.read_to_end(&mut bytes)?;
+
+    println!("Crate `{}={}` downloaded successfully", name, version);
+    Ok(bytes)
 }
